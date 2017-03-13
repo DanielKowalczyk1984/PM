0b86a1671cf347ef4eec261fde57d89f76ff9c35
DanielKowalczyk1984 <daniel.kowalczyk1984@gmail.com>
2017-03-13 22:25:55 +0100

    added individual cppchecks

--- cmake/CppcheckTargets.cmake
+++ cmake/CppcheckTargets.cmake
@@ -231,3 +231,102 @@ function(add_cppcheck _name)
 	endif()
 
 endfunction()
+
+function(add_cppcheck_indvidual _name)
+	if(NOT TARGET ${_name})
+		message(FATAL_ERROR
+			"add_cppcheck given a target name that does not exist: '${_name}' !")
+	endif()
+	if(CPPCHECK_FOUND)
+		set(_cppcheck_args)
+
+		list(FIND ARGN UNUSED_FUNCTIONS _unused_func)
+		if("${_unused_func}" GREATER "-1")
+			list(APPEND _cppcheck_args ${CPPCHECK_UNUSEDFUNC_ARG})
+		endif()
+
+		list(FIND ARGN STYLE _style)
+		if("${_style}" GREATER "-1")
+			list(APPEND _cppcheck_args ${CPPCHECK_STYLE_ARG})
+		endif()
+
+		list(FIND ARGN POSSIBLE_ERROR _poss_err)
+		if("${_poss_err}" GREATER "-1")
+			list(APPEND _cppcheck_args ${CPPCHECK_POSSIBLEERROR_ARG})
+		endif()
+
+		list(FIND ARGN FORCE _force)
+		if("${_force}" GREATER "-1")
+			list(APPEND _cppcheck_args "--force")
+		endif()
+
+		list(FIND _input FAIL_ON_WARNINGS _fail_on_warn)
+		if("${_fail_on_warn}" GREATER "-1")
+			list(APPEND
+				CPPCHECK_FAIL_REGULAR_EXPRESSION
+				${CPPCHECK_WARN_REGULAR_EXPRESSION})
+			list(REMOVE_AT _input ${_unused_func})
+		endif()
+
+		get_target_property(_cppcheck_includes "${_name}" INCLUDE_DIRECTORIES)
+		set(_includes)
+		foreach(_include ${_cppcheck_includes})
+			list(APPEND _includes "-I${_include}")
+		endforeach()
+
+		get_target_property(_cppcheck_sources "${_name}" SOURCES)
+		foreach(_source ${_cppcheck_sources})
+			set(_files)
+			get_source_file_property(_cppcheck_lang "${_source}" LANGUAGE)
+			get_source_file_property(_cppcheck_loc "${_source}" LOCATION)
+			if("${_cppcheck_lang}" MATCHES "CXX" OR "${_cppcheck_lang}" MATCHES "C")
+				list(APPEND _files "${_cppcheck_loc}")
+			endif()
+			set(_file_name)
+			get_filename_component(_file_name ${_source} NAME_WE)
+			add_custom_target(cppcheck_${_file_name})
+			set_target_properties(cppcheck_${_file_name} PROPERTIES EXCLUDE_FROM_ALL TRUE)
+			add_custom_command(TARGET
+				cppcheck_${_file_name}
+				PRE_BUILD
+				COMMAND
+				${CPPCHECK_EXECUTABLE}
+				${CPPCHECK_QUIET_ARG}
+				${CPPCHECK_TEMPLATE_ARG}
+				${_cppcheck_args}
+				${_includes}
+				${_files}
+				WORKING_DIRECTORY
+				"${CMAKE_CURRENT_SOURCE_DIR}"
+				COMMENT
+				"${_file_name}_cppcheck: Running cppcheck on target ${_file_name}..."
+				VERBATIM)
+		endforeach()
+
+		# if("1.${CMAKE_VERSION}" VERSION_LESS "1.2.8.0")
+		# 	# Older than CMake 2.8.0
+		# 	add_test(${_name}_cppcheck_test
+		# 		"${CPPCHECK_EXECUTABLE}"
+		# 		${CPPCHECK_TEMPLATE_ARG}
+		# 		${_cppcheck_args}
+		# 		${_files})
+		# else()
+		# 	# CMake 2.8.0 and newer
+		# 	add_test(NAME
+		# 		${_name}_cppcheck_test
+		# 		COMMAND
+		# 		"${CPPCHECK_EXECUTABLE}"
+		# 		${CPPCHECK_TEMPLATE_ARG}
+		# 		${_cppcheck_args}
+		# 		${_files})
+		# endif()
+
+		# set_tests_properties(${_name}_cppcheck_test
+		# 	PROPERTIES
+		# 	FAIL_REGULAR_EXPRESSION
+		# 	"${CPPCHECK_FAIL_REGULAR_EXPRESSION}")
+
+
+	endif()
+
+endfunction()
