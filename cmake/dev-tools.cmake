2fb3fa940dfc718282bec1b28970ff5b6485eb45
DanielKowalczyk1984 <daniel.kowalczyk1984@gmail.com>
2017-03-13 22:26:32 +0100

    added individual clang-format

--- cmake/dev-tools.cmake
+++ cmake/dev-tools.cmake
@@ -1,9 +1,6 @@
 # Additional targets to perform clang-format/clang-tidy
 # Get all project files
-file(GLOB_RECURSE
-     ALL_CXX_SOURCE_FILES
-     *.[chi]pp *.[chi]xx *.cc *.c *.hh *.ii *.[CHI]
-     )
+get_target_property(ALL_CXX_SOURCE_FILES ${PROJECT_NAME} SOURCES)
 
 # Adding clang-format target if executable is found
 find_program(CLANG_FORMAT "clang-format")
@@ -15,4 +12,18 @@ if(CLANG_FORMAT)
     -style=file
     ${ALL_CXX_SOURCE_FILES}
     )
+    set_target_properties(clang-format PROPERTIES EXCLUDE_FROM_ALL TRUE)
+    foreach(_file ${ALL_CXX_SOURCE_FILES})
+        get_filename_component(_name ${_file} NAME_WE)
+        add_custom_target(clang-format-${_name})
+        add_custom_command(
+            TARGET
+            clang-format-${_name}
+            COMMAND /usr/bin/clang-format
+            -i
+            -style=file
+            ${_file}
+            )
+        set_target_properties(clang-format-${_name} PROPERTIES EXCLUDE_FROM_ALL TRUE)
+    endforeach()
 endif()
