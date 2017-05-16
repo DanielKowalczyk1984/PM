ead7710b4d51eb550470f832c571b9d8184ea4a1
DanielKowalczyk1984 <daniel.kowalczyk1984@gmail.com>
2017-05-16 17:08:40 +0200

    use of pair

--- src/preprocess.c
+++ src/preprocess.c
@@ -1,9 +1,9 @@
-#include <wct.h>
 #include <interval.h>
+#include <wct.h>
 
-void g_problem_summary_init(gpointer data, gpointer user_data){
-    Job *j = (Job *) data;
-    wctproblem *prob = (wctproblem*) user_data;
+void g_problem_summary_init(gpointer data, gpointer user_data) {
+    Job *       j = (Job *)data;
+    wctproblem *prob = (wctproblem *)user_data;
 
     prob->psum += j->processingime;
     prob->pmax = CC_MAX(prob->pmax, j->processingime);
@@ -94,101 +94,115 @@ int preprocess_data(wctproblem *problem) {
     return val;
 }
 
-static int calculate_T(Job *i, Job *j, int k, GPtrArray *interval_array){
-    interval *I = (interval *) g_ptr_array_index(interval_array, k);
+static int calculate_T(interval_pair *pair, int k, GPtrArray *interval_array) {
+    interval *I = (interval *)g_ptr_array_index(interval_array, k);
+    interval * tmp;
+    Job *i = pair->a;
+    Job *j = pair->b;
+    pair->left = I->a;
+    pair->right = I->a + j->processingime;
 
-    int T = I->a;
-    if (T > I->b - i->processingime) {
-        return T;
+    if ( pair->left > I->b - i->processingime) {
+        return pair->left;
     } else {
-        T = I->a;
-
-        if(value_diff_Fij(T, i, j) <= 0) {
-            return T;
+        if (value_diff_Fij(pair->left, i, j) <= 0) {
+            return pair->left;
         }
-        for(int t = k - 1; t >= 0 ; t--) {
-            T = ((interval *) g_ptr_array_index(interval_array, t))->a + j->processingime - i->processingime;
-            if(value_diff_Fij(T, i, j) <= 0 || T == 0) {
+
+        for (int t = k - 1; t >= 0; t--) {
+            tmp = (interval *)g_ptr_array_index(interval_array, t);
+            pair->left = tmp->a + j->processingime - i->processingime;
+
+            if (value_diff_Fij(pair->left, i, j) <= 0) {
                 break;
             }
         }
 
-        return T;
+        return pair->left;
     }
 }
 
-static int check_interval(Job *i, Job *j, int k,GPtrArray *interval_array){
-    interval *I = (interval *) g_ptr_array_index(interval_array, k);
-    return (I->a + j->processingime >= I->b || calculate_T(i, j, k, interval_array) <= I->a);
+static int check_interval(interval_pair *pair, int k, GPtrArray *interval_array) {
+    interval *I = (interval *)g_ptr_array_index(interval_array, k);
+    Job *j = pair->b;
+    return (I->a + j->processingime >= I->b ||
+            calculate_T(pair, k, interval_array) <= I->a);
 }
 
-static int check_interval2(Job *i, Job *j, int k, GPtrArray *interval_array){
-    interval *I = (interval *) g_ptr_array_index(interval_array, k);
-    printf("test %d %d %d %d\n",I->a, calculate_T(i, j, k, interval_array),I->a + j->processingime, I->b);
-    printf("jobs %f %f\n",(double) i->weight/i->processingime, (double) j->weight/j->processingime );
-    return (I->a + j->processingime <= I->b && calculate_T(i, j, k, interval_array) >= I->a);
+static int check_interval2(interval_pair *pair, int k, GPtrArray *interval_array) {
+    interval *I = (interval *)g_ptr_array_index(interval_array, k);
+    Job *j = pair->b;
+    printf("test %d %d %d %d\n", I->a, calculate_T(pair, k, interval_array),
+           I->a + j->processingime, I->b);
+    return (I->a + j->processingime <= I->b &&
+            calculate_T(pair, k, interval_array) >= I->a);
 }
 
-int find_division(wctproblem *problem){
-    int val = 0;
-    int njobs = problem->njobs;
-    int tmp;
-    int prev;
+int find_division(wctproblem *problem) {
+    int        val = 0;
+    int        njobs = problem->njobs;
+    int        tmp;
+    int        prev;
     GPtrArray *tmp_array = g_ptr_array_new_with_free_func(intervals_free);
-    GPtrArray* jobarray = problem->g_job_array;
-    Job *tmp_j;
-    Job *j1,*j2;
-    interval *tmp_interval;
+    GPtrArray *jobarray = problem->g_job_array;
+    Job *      tmp_j;
+    Job *      j1, *j2;
+    interval * tmp_interval;
+    interval_pair *pair;
+    interval_pair tmp_pair;
 
     /** Find initial partition */
     prev = 0;
-    for(int i = 0; i < njobs && prev < problem->H_max; ++i) {
+    for (int i = 0; i < njobs && prev < problem->H_max; ++i) {
         tmp_j = (Job *)g_ptr_array_index(jobarray, i);
         tmp = CC_MIN(problem->H_max, tmp_j->duetime);
-        if(prev < tmp) {
+        if (prev < tmp) {
             tmp_interval = interval_alloc(prev, tmp, jobarray, njobs);
             g_ptr_array_add(tmp_array, tmp_interval);
-            CCcheck_NULL_2(tmp_interval, "Failed to allocate memory")
-            prev = tmp_j->duetime;
+            CCcheck_NULL_2(tmp_interval, "Failed to allocate memory") prev =
+                tmp_j->duetime;
         }
     }
 
-    if(prev < problem->H_max) {
+    if (prev < problem->H_max) {
         tmp_interval = interval_alloc(prev, problem->H_max, jobarray, njobs);
         g_ptr_array_add(tmp_array, tmp_interval);
     }
 
+    //g_ptr_array_foreach(tmp_array, g_print_interval, NULL);
+
     /** calculate the new intervals */
-    for(unsigned i = 0; i < tmp_array->len; ++i) {
+    for (unsigned i = 0; i < tmp_array->len; ++i) {
+        GPtrArray *pairs = g_ptr_array_new_with_free_func(interval_pair_free);
         tmp_interval = (interval *) g_ptr_array_index(tmp_array, i);
-        int count = 0;
         for (size_t j = 0; j < tmp_interval->sigma->len - 1; j++) {
             for (size_t k = j + 1; k < tmp_interval->sigma->len; k++) {
-                j1 = (Job *) g_ptr_array_index(tmp_interval->sigma, j);
-                j2 = (Job *) g_ptr_array_index(tmp_interval->sigma, k);
-                if((double)j1->weight/j1->processingime >= (double)j2->weight/j2->processingime) {
-                    if (!check_interval(j1, j2, i, tmp_array)) {
-                        count++;
-                        check_interval2(j1, j2, i,tmp_array);
-
-                    }
+                j1 = (Job *)g_ptr_array_index(tmp_interval->sigma, j);
+                j2 = (Job *)g_ptr_array_index(tmp_interval->sigma, k);
+                tmp_pair = (interval_pair) {j1, j2};
+                if (!check_interval(&tmp_pair, i, tmp_array)) {
+                    pair = CC_SAFE_MALLOC(1, interval_pair);
+                    *pair =  (interval_pair) {j1, j2, tmp_pair.left, tmp_pair.right};
+                    g_ptr_array_add(pairs, pair);
                 }
             }
         }
-        if (count) {
-            printf("count = %d \n", count);
+
+        if (pairs->len) {
+            for(unsigned l= 0; l < pairs->len; ++l) {
+                pair = (interval_pair *) g_ptr_array_index(pairs, l);
+                printf("%d %d %d %d\n", pair->a->job, pair->b->job, pair->left, pair->right);
+            }
+            printf("count = %d \n", pairs->len);
         }
+
+        g_ptr_array_free(pairs, TRUE);
     }
 
-        //
-        //intervals_free(tmp_interval);
+//
+// intervals_free(tmp_interval);
 
-
-
-
-
-
-    CLEAN:
+CLEAN:
     g_ptr_array_free(tmp_array, TRUE);
     return val;
 }
