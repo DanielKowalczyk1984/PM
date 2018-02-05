1411067e0bb3e90e6ef220d7fce03c9581a36ba7
DanielKowalczyk1984 <daniel.kowalczyk1984@gmail.com>
2018-02-05 21:22:30 +0100

    test all the alpha

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,8 +1,8 @@
-
-for m in   3; do
-for n in  100 ; do
-for file in ../$n*$m/*.txt;do
-./wct  -p 1 -l 900 -S 0 -z 1 -b 1 -Z 1 -f 10 -c 0 $file $m;
+declare -a list=($(find ~/Dropbox/PapersOR/PM/implementation/resources/instances/ -regex '.+[01]+[045]0\/.+[16]\.dat'))
+for m in   2 4; do
+for alpha in (0.7 0.75 0.8 0.85 0.9)  ; do
+for file in $list;do
+./PM  -S 1 -a $alpha -f 5  $file $m;
 echo $m;
 done
 done
