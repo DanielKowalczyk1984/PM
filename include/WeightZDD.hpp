93df695b5db2130219ccac1c20b3d6825d14150e
DanielKowalczyk1984 <daniel.kowalczyk1984@gmail.com>
2018-11-26 21:35:31 +0100

    added job to PricerWeightZDD

--- include/WeightZDD.hpp
+++ include/WeightZDD.hpp
@@ -9,6 +9,7 @@
 template <typename T> class PricerWeightZDD {
 public:
     std::vector<std::shared_ptr<node<T>>> list;
+    Job *job;
 
     PricerWeightZDD() {};
 
@@ -16,7 +17,7 @@ public:
         list.clear();
     }
 
-    std::shared_ptr<node<T>>& add_weight(int _weight, int layer, int njobs)
+    std::shared_ptr<node<T>>& add_weight(int _weight, int layer)
     {
         for (auto& it : list) {
             if (it->weight == _weight || it->terminal) {
@@ -24,51 +25,43 @@ public:
             }
         }
 
-        std::shared_ptr<node<T>> p = std::make_shared<node<T>>();
-        p->layer = layer;
+        std::shared_ptr<node<T>> p{std::make_shared<node<T>>()};
+        p->job = job;
         p->weight = _weight;
-        list.push_back(p);
+        list.push_back(std::move(p));
         return (list.back());
     }
 
-    void add_terminal_node(int j, int layer, int njobs){
-        std::shared_ptr<node<T>> p = std::make_shared<node<T>>();
-        if(j == 0){
-            p->obj = -DBL_MAX;
-        } else {
-            p->obj = -DBL_MAX;
-        }
-
-        p->take = false;
-        p->terminal = true;
-        p->layer = layer;
-        list.push_back(p);
+    void set_job(Job *_job){
+        job = _job;
     }
 
-    void init_terminal_node(int j, int njobs)
-    {
-        for (auto& it : list) {
-            it->obj = -DBL_MAX;
-            it->take = false;
-            it->prev = nullptr;
-        }
+    Job* get_job(){
+        return job;
     }
 };
 
 
 template <typename E, typename T>
 class WeightZDD : public tdzdd::DdEval<E, PricerWeightZDD<T>, Optimal_Solution<T>> {
-    T   *pi;
-    GPtrArray *interval_list;
-    int nlayers;
     int nbjobs;
+    T   *pi;
 
 public:
-    WeightZDD(T *_pi, GPtrArray *_interval_list, int _nbjobs): pi(_pi),
-        interval_list(_interval_list), nbjobs(_nbjobs)
-    {
-        nlayers = (int) interval_list->len;
+
+    WeightZDD() : nbjobs(0), pi(nullptr){
+    }
+
+    WeightZDD(T *_pi,  int _nbjobs):  nbjobs(_nbjobs), pi(_pi){
     };
+
+    WeightZDD(int _nbjobs): nbjobs(_nbjobs){
+    };
+
+    WeightZDD(const WeightZDD<E, T> &src){
+        nbjobs = src.nbjobs;
+        pi = src.pi;
+    }
 
     void evalTerminal(PricerWeightZDD<T>& n)
     {
@@ -76,19 +69,20 @@ public:
             it->obj = pi[nbjobs];
             it->c = pi[nbjobs];
             it->b = pi[nbjobs];
-            it->prev_job = nullptr;
-            it->prev_node = nullptr;
+            it->terminal = true;
         }
     }
 
-    void evalNode(PricerWeightZDD<T> *n, int i) const
+    void initializepi(T *_pi){
+        pi = _pi;
+    }
+
+    void evalNode(PricerWeightZDD<T> *n) const
     {
-        int j = nlayers - i;
-        assert(j >= 0 && j <= nlayers - 1);
-        job_interval_pair *tmp_pair = (job_interval_pair *) g_ptr_array_index(
-                                          interval_list, j);
-        Job *tmp_j = tmp_pair->j;
-        double result;
+        Job *tmp_j = n->get_job();
+        assert(tmp_j != nullptr);
+
+        T result, aux_obj0, aux_obj1;
 
         for (auto& it : n->list) {
             int      weight = it->weight;
@@ -98,31 +92,28 @@ public:
             T        obj1 = p1->obj;
             result = - value_Fj(weight + tmp_j->processingime, tmp_j) + pi[tmp_j->job];
 
-            it->obj1 = obj1 + result;
-            it->obj0 = obj0;
+            aux_obj1 = obj1 + result;
+            aux_obj0 = obj0;
 
             if ((it->calc)) {
-                if (it->obj0 < it->obj1) {
-                    it->obj = it->obj1;
+                if (aux_obj0 < aux_obj1) {
+                    it->obj = aux_obj1;
                     it->take = true;
-                    it->prev_job = tmp_j;
                 } else {
-                    it->obj = it->obj0;
+                    it->obj = aux_obj0;
                     it->take = false;
-                    it->prev_job = p0->prev_job;
                 }
 
-                if (it->b < it->obj1) {
-                    it->b = it->obj1;
+                if (it->b < aux_obj1) {
+                    it->b = aux_obj1;
                 }
 
-                if(it->c < it->obj0) {
-                    it->c = it->obj0;
+                if(it->c < aux_obj0) {
+                    it->c = aux_obj0;
                 }
             } else {
-                it->obj = it->obj0;
+                it->obj = aux_obj0;
                 it->take = false;
-                it->prev_job = p0->prev_job;
             }
         }
     }
@@ -135,37 +126,32 @@ public:
             it->c = pi[nbjobs];
             it->dist = -DBL_MAX;
             it->take = false;
+            it->terminal = false;
         }
     }
 
-    Optimal_Solution<T> get_objective(tdzdd::NodeTableHandler<2> diagram,
-                                      tdzdd::DataTable<PricerWeightZDD<T>> *data_table,
-                                      const tdzdd::NodeId *f)
+    Optimal_Solution<T> get_objective(PricerWeightZDD<T> &n)
     {
         Optimal_Solution<T> sol;
-        sol.obj = (*data_table)[f->row()][f->col()].list[sol.C_max]->obj;
-        std::shared_ptr<node<T>> ptr = (*data_table)[f->row()][f->col()].list[sol.C_max];
-        job_interval_pair *tmp_pair;
-        Job *tmp_j;
+        std::shared_ptr<node<T>> ptr = n.list.front();
+        sol.obj = pi[nbjobs];
 
-
-        while (ptr->layer != nlayers) {
-            tmp_pair = (job_interval_pair *) g_ptr_array_index(interval_list, ptr->layer);
-            tmp_j = tmp_pair->j;
-
-            if (ptr->obj == ptr->obj1) {
+        do {
+            Job *tmp_j = ptr->job;
+            if (ptr->take) {
                 g_ptr_array_add(sol.jobs, tmp_j);
                 // auto e = ptr->out_edge[0].lock();
                 // g_ptr_array_add(sol.e_list, &(e->id));
                 sol.C_max += tmp_j->processingime;
                 sol.cost += value_Fj(sol.C_max, tmp_j);
+                sol.obj += -value_Fj(sol.C_max, tmp_j) + pi[tmp_j->job];
                 ptr = ptr->y;
             } else {
                 // auto e = ptr->out_edge[1].lock();
                 // g_ptr_array_add(sol.e_list, &(e->id));
                 ptr = ptr->n;
             }
-        }
+        }while (ptr->job);
 
         return sol;
     }
