953a1263423551395087ea598598fb5d40e4048e
DanielKowalczyk1984 <daniel.kowalczyk1984@gmail.com>
2018-12-04 10:26:12 +0100

    changed name of evaluator + added simple evaluation for Zdd

--- include/DurationZDD.hpp
+++ include/DurationZDD.hpp
@@ -1,35 +1,33 @@
 #ifndef DURATION_ZDD_HPP
 #define DURATION_ZDD_HPP
 #include <tdzdd/DdEval.hpp>
-
 #include <OptimalSolution.hpp>
 #include <node_duration.hpp>
-
 #include <vector>
 #include <algorithm>
 using namespace std;
 
 template<typename T>
-class PricerDurationZDD {
+class ForwardZddNode {
   public:
-    std::vector<std::shared_ptr<NodeDuration<T>>>  list;
+    std::vector<std::shared_ptr<Node<T>>>  list;
     Job *job;
 
-    PricerDurationZDD() {}
+    ForwardZddNode() {}
 
-    ~PricerDurationZDD() {
+    ~ForwardZddNode() {
         list.clear();
     }
 
 
-    std::shared_ptr<NodeDuration<T>> add_weight(int _weight, int _layer, bool _rootnode = false, bool _terminal_node = false){
+    std::shared_ptr<Node<T>> add_weight(int _weight, int _layer, bool _rootnode = false, bool _terminal_node = false){
         for (auto &it : list) {
             if (it->GetWeight() == _weight) {
                 return it;
             }
         }
 
-        std::shared_ptr<NodeDuration<T>> node = std::make_shared<NodeDuration<T>>(_weight, _layer, _rootnode, _terminal_node);
+        std::shared_ptr<Node<T>> node = std::make_shared<Node<T>>(_weight, _layer, _rootnode, _terminal_node);
         node->set_job(job);
         list.push_back(std::move(node));
         return list.back();
@@ -45,34 +43,106 @@ class PricerDurationZDD {
 };
 
 template<typename T>
-bool my_compare(const std::shared_ptr<NodeDuration<T>> &lhs, const std::shared_ptr<NodeDuration<T>> &rhs){
+bool my_compare(const std::shared_ptr<Node<T>> &lhs, const std::shared_ptr<Node<T>> &rhs){
     return *lhs < *rhs;
 }
 
-template<typename E, typename T> class DurationZDD : public
-    tdzdd::DdEval<E, PricerDurationZDD<T>, Optimal_Solution<T>> {
-  private:
+template<typename E, typename T> class ForwardZddBase : public 
+    tdzdd::DdEval<E, ForwardZddNode<T>, Optimal_Solution<T>> {
+protected:
     T *pi;
     int num_jobs;
-  public:
-    DurationZDD(T *_pi, int _num_jobs): pi(_pi), num_jobs(_num_jobs) {
+public:
+    ForwardZddBase(T *_pi, int _num_jobs): pi(_pi), num_jobs(_num_jobs) {
     }
 
-    DurationZDD(int _num_jobs)
+    ForwardZddBase(int _num_jobs)
     :  num_jobs(_num_jobs){
     }
 
-    DurationZDD(){
+    ForwardZddBase(){
         pi = nullptr;
         num_jobs = 0;
     }
 
-    DurationZDD(const DurationZDD<E, T> &src) {
+    ForwardZddBase(const ForwardZddBase<E, T> &src) {
         pi = src.pi;
         num_jobs = src.num_jobs;
     }
 
-    void initializenode(PricerDurationZDD<T>& n){
+    void initializepi(T *_pi){
+        pi = _pi;
+    }
+
+    virtual void initializenode(ForwardZddNode<T>& n) const  = 0;
+
+    virtual void initializerootnode(ForwardZddNode<T>& n) const  = 0;
+
+    virtual void evalNode(ForwardZddNode<T>& n) const = 0;
+
+    // virtual Optimal_Solution<double> get_objective(ForwardZddNode<T>& n) const = 0;
+
+    Optimal_Solution<T> get_objective(ForwardZddNode<T> &n) const {
+        Optimal_Solution<T> sol;
+        Job *aux_job;
+
+        sol.cost = 0;
+        sol.C_max = 0;
+        sol.obj = pi[num_jobs];
+        int weight;
+
+
+        auto m =  std::max_element(n.list.begin(), n.list.end(),my_compare<T>);
+        weight = (*m)->GetWeight();
+
+        PrevNode<T> *ptr_node = &((*m)->prev1);
+
+        while(ptr_node->GetPrev() != nullptr) {
+            PrevNode<T> *aux_prev_node = ptr_node->GetPrev();
+            aux_job = aux_prev_node->GetJob();
+            if(ptr_node->GetHigh()) {
+                sol.C_max += aux_job->processingime;
+                if(ptr_node->GetNode()) {
+                    sol.cost = sol.cost + value_Fj(ptr_node->GetWeight(), aux_job) ;
+                    sol.obj += pi[aux_job->job] - value_Fj(ptr_node->GetWeight(), aux_job);
+                } else {
+                    sol.cost = sol.cost + value_Fj(weight, aux_job);
+                    sol.obj += pi[aux_job->job] - value_Fj(weight, aux_job);
+                }
+                g_ptr_array_insert(sol.jobs, 0, aux_job);
+            }
+            ptr_node = aux_prev_node;
+        }
+
+        assert(sol.C_max == weight);
+
+        return sol;
+    }
+
+};
+
+template<typename E, typename T> class ForwardZddCycle : public ForwardZddBase<E, T> {
+
+  public:
+    using ForwardZddBase<E, T>::pi;
+    using ForwardZddBase<E, T>::num_jobs;
+    ForwardZddCycle(T *_pi, int _num_jobs): ForwardZddBase<E, T>(_pi, _num_jobs) {
+    }
+
+    ForwardZddCycle(int _num_jobs) : ForwardZddBase<E, T>(_num_jobs){
+    }
+
+    ForwardZddCycle(): ForwardZddBase<E, T>(){
+        pi = nullptr;
+        num_jobs = 0;
+    }
+
+    ForwardZddCycle(const ForwardZddCycle<E, T> &src) {
+        pi = src.pi;
+        num_jobs = src.num_jobs;
+    }
+
+    void initializenode(ForwardZddNode<T>& n) const {
         for (auto &it: n.list) {
             if(it->GetWeight() == 0) {
                 it->prev1.UpdateSolution(pi[num_jobs], nullptr, false);
@@ -84,18 +154,14 @@ template<typename E, typename T> class DurationZDD : public
         }
     }
 
-    void initializerootnode(PricerDurationZDD<T> &n) {
+    void initializerootnode(ForwardZddNode<T> &n) const {
         for(auto &it: n.list){
             it->prev1.f = pi[num_jobs];
             it->prev2.SetF(-DBL_MAX/2);
         }
     }
 
-    void initializepi(T *_pi){
-        pi = _pi;
-    }
-
-    void evalNode(PricerDurationZDD<T> &n) const
+    void evalNode(ForwardZddNode<T> &n) const
     {
         Job *tmp_j = n.get_job();
         assert(tmp_j != nullptr);
@@ -105,8 +171,8 @@ template<typename E, typename T> class DurationZDD : public
         for (auto &it : n.list) {
             int      weight = it->GetWeight();
             T g;
-            std::shared_ptr<NodeDuration<T>> p0 = it->n;
-            std::shared_ptr<NodeDuration<T>> p1 = it->y;
+            std::shared_ptr<Node<T>> p0 = it->n;
+            std::shared_ptr<Node<T>> p1 = it->y;
             result = - value_Fj(weight + tmp_j->processingime, tmp_j) + pi[tmp_j->job];
 
             /**
@@ -153,48 +219,85 @@ template<typename E, typename T> class DurationZDD : public
         }
     }
 
-
-    Optimal_Solution<T> get_objective(PricerDurationZDD<T> &n) {
+    Optimal_Solution<T> getValue(ForwardZddNode<T> const &n){
         Optimal_Solution<T> sol;
-        Job *aux_job;
 
-        sol.cost = 0;
-        sol.C_max = 0;
-        sol.obj = pi[num_jobs];
-        int weight;
+        return sol;
+    }
+};
 
+template<typename E, typename T> class ForwardZddSimple : public ForwardZddBase<E, T> {
+  public:
+    using ForwardZddBase<E, T>::pi;
+    using ForwardZddBase<E, T>::num_jobs;
+    ForwardZddSimple(T *_pi, int _num_jobs): ForwardZddBase<E, T>(_pi, _num_jobs) {
+    }
 
-        auto m =  std::max_element(n.list.begin(), n.list.end(),my_compare<T>);
-        weight = (*m)->GetWeight();
+    ForwardZddSimple(int _num_jobs)
+    :  ForwardZddBase<E, T> (_num_jobs){
+    }
 
-        PrevNode<T> *ptr_node = &((*m)->prev1);
+    ForwardZddSimple(){
+        pi = nullptr;
+        num_jobs = 0;
+    }
 
-        while(ptr_node->GetPrev() != nullptr) {
-            PrevNode<T> *aux_prev_node = ptr_node->GetPrev();
-            aux_job = aux_prev_node->GetJob();
-            if(ptr_node->GetHigh()) {
-                sol.C_max += aux_job->processingime;
-                if(ptr_node->GetNode()) {
-                    sol.cost = sol.cost + value_Fj(ptr_node->GetWeight(), aux_job) ;
-                    sol.obj += pi[aux_job->job] - value_Fj(ptr_node->GetWeight(), aux_job);
-                } else {
-                    sol.cost = sol.cost + value_Fj(weight, aux_job);
-                    sol.obj += pi[aux_job->job] - value_Fj(weight, aux_job);
-                }
-                g_ptr_array_insert(sol.jobs, 0, aux_job);
-                // g_ptr_array_add(sol.jobs, aux_job);
+    ForwardZddSimple(const ForwardZddSimple<E, T> &src) {
+        pi = src.pi;
+        num_jobs = src.num_jobs;
+    }
+
+    void initializenode(ForwardZddNode<T>& n) const {
+        for (auto &it: n.list) {
+            if(it->GetWeight() == 0) {
+                it->prev1.UpdateSolution(pi[num_jobs], nullptr, false);
+            } else {
+                it->prev1.UpdateSolution(-DBL_MAX/2, nullptr, false);
             }
-            ptr_node = aux_prev_node;
         }
+    }
 
-        assert(sol.C_max == weight);
-        // printf("test cost %f %f\n", sol.obj, (*m)->prev1.GetF());
-        // for(int i = 0; i < sol.jobs->len; i++){
-        //     Job *tmp_job = (Job *) g_ptr_array_index(sol.jobs, i);
-        //     printf("%d ", tmp_job->job);
-        // }
-        // printf("\n");
+    void initializerootnode(ForwardZddNode<T> &n) const {
+        for(auto &it: n.list){
+            it->prev1.f = pi[num_jobs];
+        }
+    }
 
+    void initializepi(T *_pi){
+        pi = _pi;
+    }
+
+    void evalNode(ForwardZddNode<T> &n) const {
+        Job *tmp_j = n.get_job();
+        assert(tmp_j != nullptr);
+        double result;
+
+        for (auto &it : n.list) {
+            int      weight = it->GetWeight();
+            T g;
+            std::shared_ptr<Node<T>> p0 = it->n;
+            std::shared_ptr<Node<T>> p1 = it->y;
+            result = - value_Fj(weight + tmp_j->processingime, tmp_j) + pi[tmp_j->job];
+
+            /**
+             * High edge calculation
+             */
+            g = it->prev1.GetF() + result;
+            if(g > p1->prev1.GetF()) {
+                p1->prev1.UpdateSolution(g, &(it->prev1), true);
+            }
+
+            /**
+             * Low edge calculation
+             */
+            if(it->prev1.GetF() > p0->prev1.GetF()) {
+                p0->prev1.UpdateSolution(it->prev1);
+            }
+        }
+    }
+
+    Optimal_Solution<T> getValue(ForwardZddNode<T> const &n){
+        Optimal_Solution<T> sol;
         return sol;
     }
 };
