c12d7021e35a38ee4e1299a1c65afe49635a9117
DanielKowalczyk1984 <daniel.kowalczyk1984@gmail.com>
2018-12-05 17:15:01 +0100

    added the structs to the pricerevaluate associated to backward evaluation

--- include/PricerEvaluate.hpp
+++ include/PricerEvaluate.hpp
@@ -1,10 +1,11 @@
 #include <ForwardBDD.hpp>
 #include <ForwardZDD.hpp>
+#include <BackwardBDD.hpp>
 
 struct ForwardZddCycleDouble : ForwardZddCycle<ForwardZddCycleDouble, double> {
     ForwardZddCycleDouble(double *_pi, int _num_jobs)
     : ForwardZddCycle<ForwardZddCycleDouble, double>(_pi, _num_jobs) {};
-    ForwardZddCycleDouble(int _num_jobs)
+    explicit ForwardZddCycleDouble(int _num_jobs)
     : ForwardZddCycle<ForwardZddCycleDouble, double> (_num_jobs) {};
     ForwardZddCycleDouble() : ForwardZddCycle<ForwardZddCycleDouble, double>() {};
 };
@@ -12,7 +13,7 @@ struct ForwardZddCycleDouble : ForwardZddCycle<ForwardZddCycleDouble, double> {
 struct ForwardZddSimpleDouble : ForwardZddSimple<ForwardZddSimpleDouble, double> {
     ForwardZddSimpleDouble(double *_pi, int _num_jobs)
     : ForwardZddSimple<ForwardZddSimpleDouble, double>(_pi, _num_jobs) {};
-    ForwardZddSimpleDouble(int _num_jobs)
+    explicit ForwardZddSimpleDouble(int _num_jobs)
     : ForwardZddSimple<ForwardZddSimpleDouble, double> (_num_jobs) {};
     ForwardZddSimpleDouble() : ForwardZddSimple<ForwardZddSimpleDouble, double>() {};
 };
@@ -20,7 +21,7 @@ struct ForwardZddSimpleDouble : ForwardZddSimple<ForwardZddSimpleDouble, double>
 struct ForwardBddSimpleDouble : ForwardBddSimple<ForwardBddSimpleDouble, double> {
     ForwardBddSimpleDouble(double *_pi, int _num_jobs)
     : ForwardBddSimple<ForwardBddSimpleDouble, double>(_pi, _num_jobs) {};
-    ForwardBddSimpleDouble(int _num_jobs)
+    explicit ForwardBddSimpleDouble(int _num_jobs)
     : ForwardBddSimple<ForwardBddSimpleDouble, double> (_num_jobs) {};
     ForwardBddSimpleDouble() : ForwardBddSimple<ForwardBddSimpleDouble, double>() {};
 };
@@ -28,8 +29,23 @@ struct ForwardBddSimpleDouble : ForwardBddSimple<ForwardBddSimpleDouble, double>
 struct ForwardBddCycleDouble : ForwardBddCycle<ForwardBddCycleDouble, double> {
     ForwardBddCycleDouble(double *_pi, int _num_jobs)
     : ForwardBddCycle<ForwardBddCycleDouble, double>(_pi, _num_jobs) {};
-    ForwardBddCycleDouble(int _num_jobs)
+    explicit ForwardBddCycleDouble(int _num_jobs)
     : ForwardBddCycle<ForwardBddCycleDouble, double> (_num_jobs) {};
     ForwardBddCycleDouble() : ForwardBddCycle<ForwardBddCycleDouble, double>() {};
 };
 
+struct BackwardBddSimpleDouble : BackwardBddSimple<BackwardBddSimpleDouble, double> {
+    BackwardBddSimpleDouble(double *_pi, int _num_jobs)
+    : BackwardBddSimple<BackwardBddSimpleDouble, double>(_pi, _num_jobs) {};
+    explicit BackwardBddSimpleDouble(int _num_jobs)
+    : BackwardBddSimple<BackwardBddSimpleDouble, double> (_num_jobs) {};
+    BackwardBddSimpleDouble() : BackwardBddSimple<BackwardBddSimpleDouble, double>() {};
+};
+
+struct BackwardBddCycleDouble : BackwardBddCycle<BackwardBddCycleDouble, double> {
+    BackwardBddCycleDouble(double *_pi, int _num_jobs)
+    : BackwardBddCycle<BackwardBddCycleDouble, double>(_pi, _num_jobs) {};
+    explicit BackwardBddCycleDouble(int _num_jobs)
+    : BackwardBddCycle<BackwardBddCycleDouble, double> (_num_jobs) {};
+    BackwardBddCycleDouble() : BackwardBddCycle<BackwardBddCycleDouble, double>() {};
+};
