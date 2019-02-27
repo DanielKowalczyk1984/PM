dde0b2ccae9f1678b98f10d17650e2401ee116b1
DanielKowalczyk1984 <daniel.kowalczyk1984@gmail.com>
2019-02-27 10:50:16 +0100

    added vertex and edge writer

--- include/MipGraph.hpp
+++ include/MipGraph.hpp
@@ -1,9 +1,11 @@
 #ifndef MIP_GRAPH_HPP
 #define MIP_GRAPH_HPP
 
+#include <ostream>
 #include <boost/graph/graph_traits.hpp>
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/unordered_set.hpp>
+#include <NodeBddTable.hpp>
 #include <node_duration.hpp>
 #include <gurobi_c++.h>
 
@@ -30,5 +32,45 @@ typedef property_map<MipGraph, vertex_name_t>::type NodeIdAccessor;
 typedef property_map<MipGraph, edge_index_t>::type EdgeIndexAccessor;
 typedef property_map<MipGraph, edge_weight_t>::type EdgeTypeAccessor;
 typedef property_map<MipGraph, edge_weight2_t>::type EdgeVarAccessor;
+
+class ColorWriterEdge {
+private:
+    const MipGraph& g;
+    const double *x;
+
+public:
+    explicit ColorWriterEdge(MipGraph& _g, double *_x) : g{_g}, x{_x} {
+
+    }
+
+    void operator()(std::ostream &output, Edge _edge) {       
+        if(x[get(boost::edge_index_t(),g,_edge)] > 0.00001) {
+            if(get(boost::edge_index_t(),g,_edge)%2 == 0) {
+                output << "[label = "<< x[get(boost::edge_index_t(),g,_edge)] <<",color = red]";
+            } else {
+                output << "[label = "<< x[get(boost::edge_index_t(),g,_edge)] <<",color = red, style = dashed]";
+            }
+        } else {
+            output << "[label = "<< x[get(boost::edge_index_t(),g,_edge)] <<"]";
+        }
+    }
+};
+
+class ColorWriterVertex {
+private:
+    const MipGraph &g;
+    NodeTableEntity<>& table;
+public:
+    ColorWriterVertex(MipGraph &_g, NodeTableEntity<>& _table) : g{_g}, table{_table} {
+
+    }
+
+    void operator()(std::ostream &output, Vertex _vertex) {
+        nodeid tmp_nodeid = get(boost::vertex_name_t(), g, _vertex);
+        if(tmp_nodeid > 1) {
+            output <<  " [label=\" " << table.node(tmp_nodeid).GetJob()->job << " " << table.node(tmp_nodeid).GetWeight()  << "\"]";
+        }
+    }
+};
 
 #endif // MIP_GRAPH_HPP
