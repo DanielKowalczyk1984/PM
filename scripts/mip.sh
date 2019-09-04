cdd42e2f1982e309eed0ddfd29529b7d2d35f135
DanielKowalczyk1984 <daniel.kowalczyk1984@gmail.com>
2019-09-04 16:19:00 +0200

    all instances in parallel

--- scripts/mip.sh
+++ scripts/mip.sh
@@ -7,10 +7,10 @@ SRC_DIR=`dirname "$BASH_SOURCE"`/..
 # Invoke MIP on the rdlp instance files in paralel. All output is collected in a single output file
 # Only output lines marked with a 'tag' are preserved in the final output. This is convenient to remove debug output.
 function runBenchmark(){
-    instances=($(ls /home/daniel/Dropbox/PapersOR/PM/implementation/instances/wt100/*{1,6}.dat))
+    instances=($(ls /home/daniel/Dropbox/PapersOR/PM/implementation/instances/wt{40,50,100,150}/*{1,6}.dat))
     machines=(2 4)
     solvers=($(seq 10))
-    printf "%s\n" "${instances[@]}" | xargs -I{} printf "{} %s\n" "${machines[@]}" | xargs -I{} printf "-a %s {}\n" "${solvers[@]}" | parallel --no-notice -P 4 --eta --colsep ' ' "build/PMRelease -f 4 -S 1 {}";
+    printf "%s\n" "${instances[@]}" | xargs -I{} printf "{} %s\n" "${machines[@]}" | xargs -I{} printf "-a %s {}\n" "${solvers[@]}" | parallel --no-notice -P 6 --eta --colsep ' ' "build/PMRelease -f 4 -S 1 {}";
 }
 
 
