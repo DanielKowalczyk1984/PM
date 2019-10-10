608a29797232cca7db23e4946420c1ab462eea2f
DanielKowalczyk1984 <daniel.kowalczyk1984@gmail.com>
2019-10-10 14:33:06 +0200

    solved problem with mip statistics

--- src/PricerSolverBase.cpp
+++ src/PricerSolverBase.cpp
@@ -78,7 +78,17 @@ double PricerSolverBase::get_dbl_attr_model(enum MIP_Attr c) {
             case MIP_Attr_Mip_Gap:
                 val = model->get(GRB_DoubleAttr_MIPGap);
                 break;
+            case MIP_Attr_Run_Time:
+                val = model->get(GRB_DoubleAttr_Runtime);
+                break;
+            case MIP_Attr_Nb_Simplex_Iter:
+                val = model->get(GRB_DoubleAttr_IterCount);
+                break;
+            case MIP_Attr_Nb_Nodes:
+                val = model->get(GRB_DoubleAttr_NodeCount);
+                break;
             default:
+                val = std::numeric_limits<double>::max();
                 break;
         }
     } else {
