369d51fd29613a4e6904f07d576a31992d7c395c
DanielKowalczyk1984 <daniel.kowalczyk1984@gmail.com>
2019-11-27 13:32:37 +0100

    calculate h_min

--- src/preprocess.c
+++ src/preprocess.c
@@ -37,23 +37,15 @@ gint g_job_compare_edd(const void* a, const void* b, void* data) {
     return (0);
 }
 
-int calculate_H_min(int* durations, int nmachines, int njobs, int* perm,
-                    double* H) {
-    int    i, val = 0;
-    double temp;
+int g_compara_duration(gconstpointer a,gconstpointer b) {
+    const Job* x = *((Job* const*)a);
+    const Job* y = *((Job* const*)b);
 
-    for (i = 0; i < njobs; ++i) {
-        val += durations[i];
+    if(x->processing_time < y->processing_time) {
+        return -1;
+    } else {
+        return 1;
     }
-
-    for (i = 0; i < nmachines - 1; ++i) {
-        val -= durations[perm[i]];
-    }
-
-    temp = (double)val;
-    *H = temp / (double)nmachines;
-    val = (int)floor(*H);
-    return val;
 }
 
 void calculate_Hmax(Problem* problem) {
@@ -64,10 +56,32 @@ void calculate_Hmax(Problem* problem) {
     temp = problem->p_sum - problem->pmax;
     temp_dbl = (double)temp;
     temp_dbl = floor(temp_dbl / problem->nb_machines);
-    problem->H_max = pd->H_max = (int)temp_dbl + problem->pmax + 10;
+    problem->H_max = pd->H_max= (int)temp_dbl + problem->pmax;
     problem->H_min = (int)ceil(temp_dbl / problem->nb_machines) - problem->pmax;
-    printf("H_max = %d,  pmax = %d, pmin = %d, psum = %d, off = %d\n",
-           problem->H_max, problem->pmax, problem->pmin, problem->p_sum,
+
+    GPtrArray *duration = g_ptr_array_new();
+    for(int j = 0; j < problem->nb_jobs;j++) {
+        Job* job = g_ptr_array_index(problem->g_job_array, j);
+        g_ptr_array_add(duration, job);
+    }
+    g_ptr_array_sort(duration,g_compara_duration);
+
+    int m = 0;
+    int i = problem->nb_jobs - 1;
+    double tmp = problem->p_sum;
+    problem->H_min = problem->p_sum;
+    do{ 
+        Job* job = g_ptr_array_index(duration, i);
+        tmp -= job->processing_time;
+        m++;
+        i--;
+
+    } while (m < problem->nb_machines - 1);
+
+    problem->H_min = (int) ceil(tmp/problem->nb_machines);
+    g_ptr_array_free(duration, TRUE);
+    printf("H_max = %d, H_min = %d,  pmax = %d, pmin = %d, psum = %d, off = %d\n",
+           problem->H_max, problem->H_min, problem->pmax, problem->pmin, problem->p_sum,
            problem->off);
 }
 
