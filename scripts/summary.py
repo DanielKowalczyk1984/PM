7deb1de585913112d5f4829ec3ce3231652fbecf
Kowalczyk, D <d.kowalczyk@tue.nl>
2020-05-06 19:02:54 +0200

    added control to summary

--- scripts/summary.py
+++ scripts/summary.py
@@ -1,10 +1,7 @@
-# To add a new cell, type '#%%'
-# To add a new markdown cell, type '#%% [markdown]'
-#%% Change working directory from the workspace root to the ipynb file location. Turn this addition off with the DataScience.changeDirOnImportExport setting
-# ms-python.python added
+#%%
 import os
 try:
-	os.chdir("/home/daniel/")
+	os.chdir("/home/danielk/")
 	print(os.getcwd())
 except:
 	pass
@@ -16,56 +13,55 @@ import re
 
 
 #%%
-file_name = "CG_overall_20190829.csv"
-data = pd.read_csv("/home/daniel/Dropbox/PapersOR/PM/implementation/results/CG_results_20190829/CG_overall_20190829.csv")
+file_name = "CG_overall_20200506.csv"
+data = pd.read_csv("/home/danielk/Dropbox/PapersOR/PM/implementation/" + file_name)
 match = re.search(r'.*\_(\d{4})(\d{2})(\d{2})\.csv',file_name)
 year = match.group(1)
 month = match.group(2)
 day = match.group(3)
 
-
-#%%
 data['gap'] = (data['global_upper_bound'] - data['global_lower_bound']
                )/(data['global_lower_bound'] + 0.00001)
 data['opt'] = data['global_lower_bound'] == data['global_upper_bound']
 data['reduction'] = (data['first_size_graph'] - data['size_after_reduced_cost'])/(data['first_size_graph'] + 0.000001)
-lst = []
-import re
-for l in data['NameInstance']:
-    test = re.search(r'.*\_(\d+)', l).group(1)
-    lst.append(int(test))
-
-se = pd.Series(lst)
-data['Id'] = se.values
+data['Inst'] = data.NameInstance.apply(
+	lambda x:  int(re.search(r'.*\_(\d+)', x).group(1)))
 
 
 #%%
 summary_grouped = data.groupby(['pricing_solver','n','m'])
-
-
-#%%
 aggregation = {"tot_lb": {np.max, np.mean},
                "gap": {np.max, np.mean},
                "first_size_graph": {np.max, np.mean},
                "opt": np.sum,
                "reduction": {np.max,np.mean},
               "tot_cputime" : {np.max, np.mean}}
-
-#%%
 summary_write = summary_grouped.agg(aggregation).pivot_table(index=['n', 'm'], values=[
                               'tot_lb', 'gap', 'first_size_graph','reduction','opt'], columns=['pricing_solver'])
-summary_write.columns.set_levels(['AFFS','AFFC','AFBS','AFBC','AFZFS','AFZFC','AFZBS','AFZBC','TI','ATI'],level=2,inplace=True)
+summary_write.columns.set_levels(['AFBC','TI','ATI','TI_BDD'],level=2,inplace=True)
 summary_write.columns = ["_".join(x) for x in summary_write.columns.ravel()]
 summary_write.to_csv("CG_summary_"+year+month+day+".csv")
 
 #%%
-all_instances = data.pivot_table(values=['tot_lb', 'gap', 'first_size_graph','reduction','opt','rel_error','nb_generated_col','global_lower_bound','global_upper_bound','tot_cputime'], index=['n','m','Id'], columns=['pricing_solver'])
-all_instances.columns.set_levels(['AFFS','AFFC','AFBS','AFBC','AFZFS','AFZFC','AFZBS','AFZBC','TI','ATI'],level=1,inplace=True)
+all_instances = data.pivot_table(values=['tot_lb', 'gap', 'first_size_graph','reduction','opt','rel_error','nb_generated_col','global_lower_bound','global_upper_bound','tot_cputime'], index=['n','m','Inst'], columns=['pricing_solver'])
+all_instances.columns.set_levels(['AFBC','TI','ATI','TI_BDD'],level=1,inplace=True)
 all_instances.columns = ["_".join(x) for x in all_instances.columns.ravel()]
 all_instances.to_csv("CG_allinstances_"+year+month+day+".csv")
 
+#%%
+df_pessoa = pd.read_csv("/home/danielk/Dropbox/PapersOR/PM/implementation/results/all_pessoa.csv")
+df_pessoa.Opt= df_pessoa.Opt.apply(str)
+df_pessoa['best'] = df_pessoa.apply(lambda x: re.search(r'[^0-9]?(\d+)', x['Opt']).group(1),axis=1)
+df_pessoa.best = df_pessoa.best.apply(pd.to_numeric)
+
+# %%
+df_all = pd.merge(data, df_pessoa, on=['Inst', 'n','m'])
+
+# %%
+df_all.loc[df_all['global_lower_bound'] > df_all['best']]
 
 #%%
+df_all.info()
 
-
-
+#%%
+df_all['NameInstance'].value_counts()
