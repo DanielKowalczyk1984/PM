5a19589e5a6e5b433224a3707741ae1a4168ea8e
Kowalczyk, D <d.kowalczyk@tue.nl>
2020-07-15 21:07:16 +0200

    format NodeId.hpp

--- include/NodeId.hpp
+++ include/NodeId.hpp
@@ -1,8 +1,8 @@
 #ifndef NODE_ID_HPP
 #define NODE_ID_HPP
 
-#include <cassert>
 #include <stdint.h>
+#include <cassert>
 #include <ostream>
 
 int const NODE_ROW_BITS = 20;
@@ -21,99 +21,68 @@ uint64_t const NODE_ATTR_MASK = uint64_t(1) << NODE_ATTR_OFFSET;
 class NodeId {
     uint64_t code_;
 
-public:
-    NodeId() { // 'code_' is not initialized in the default constructor for SPEED.
+   public:
+    NodeId() {  // 'code_' is not initialized in the default constructor for
+                // SPEED.
     }
 
-    NodeId(uint64_t code) :
-            code_(code) {
-    }
+    NodeId(uint64_t code) : code_(code) {}
 
-    NodeId(uint64_t row, uint64_t col) :
-            code_((row << NODE_ROW_OFFSET) | col) {
-    }
+    NodeId(uint64_t row, uint64_t col)
+        : code_((row << NODE_ROW_OFFSET) | col) {}
 
-    NodeId(uint64_t row, uint64_t col, bool attr) :
-            code_((row << NODE_ROW_OFFSET) | col) {
+    NodeId(uint64_t row, uint64_t col, bool attr)
+        : code_((row << NODE_ROW_OFFSET) | col) {
         setAttr(attr);
     }
 
-    int row() const {
-        return code_ >> NODE_ROW_OFFSET;
-    }
+    int row() const { return code_ >> NODE_ROW_OFFSET; }
 
-    size_t col() const {
-        return code_ & NODE_COL_MAX;
-    }
+    size_t col() const { return code_ & NODE_COL_MAX; }
 
     void setAttr(bool val) {
         if (val) {
             code_ |= NODE_ATTR_MASK;
-        }
-        else {
+        } else {
             code_ &= ~NODE_ATTR_MASK;
         }
     }
 
-    bool getAttr() const {
-        return (code_ & NODE_ATTR_MASK) != 0;
-    }
+    bool getAttr() const { return (code_ & NODE_ATTR_MASK) != 0; }
 
-    NodeId withoutAttr() const {
-        return code_ & ~NODE_ATTR_MASK;
-    }
+    NodeId withoutAttr() const { return code_ & ~NODE_ATTR_MASK; }
 
-    bool hasEmpty() const {
-        return code_ == 1 || getAttr();
-    }
+    bool hasEmpty() const { return code_ == 1 || getAttr(); }
 
-    uint64_t code() const {
-        return code_ & ~NODE_ATTR_MASK;
-    }
+    uint64_t code() const { return code_ & ~NODE_ATTR_MASK; }
 
-    size_t hash() const {
-        return code() * 314159257;
-    }
+    size_t hash() const { return code() * 314159257; }
 
-    bool operator==(NodeId const& o) const {
-        return code() == o.code();
-    }
+    bool operator==(NodeId const& o) const { return code() == o.code(); }
 
-    bool operator!=(NodeId const& o) const {
-        return !(*this == o);
-    }
+    bool operator==(NodeId const && o) const {return code() == o.code();}
 
-    bool operator<(NodeId const& o) const {
-        return code() < o.code();
-    }
+    bool operator!=(NodeId const& o) const { return !(*this == o); }
 
-    bool operator>=(NodeId const& o) const {
-        return !(*this < o);
-    }
+    bool operator<(NodeId const& o) const { return code() < o.code(); }
 
-    bool operator>(NodeId const& o) const {
-        return o < *this;
-    }
+    bool operator>=(NodeId const& o) const { return !(*this < o); }
 
-    bool operator<=(NodeId const& o) const {
-        return !(o < *this);
-    }
+    bool operator>(NodeId const& o) const { return o < *this; }
 
-    NodeId& operator=(const NodeId &src){
-      if(&src == this) {
-        return *this;
-      }
+    bool operator<=(NodeId const& o) const { return !(o < *this); }
 
-      code_ = src.code_;
-
-      return *this;
-    }
+    NodeId& operator=(const NodeId&) = default;
+    NodeId(const NodeId&) = default;
+    NodeId(NodeId&&) = default;
+    NodeId& operator=(NodeId&&) = default;
 
     friend std::ostream& operator<<(std::ostream& os, NodeId const& o) {
         os << o.row() << ":" << o.col();
-        if (o.code_ & NODE_ATTR_MASK) os << "+";
+        if (o.code_ & NODE_ATTR_MASK)
+            os << "+";
         return os;
     }
 };
 
-#endif // NODE_ID_HPP
+#endif  // NODE_ID_HPP
