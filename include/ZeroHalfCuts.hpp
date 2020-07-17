a372060397b53bdcabaf54458ac8686a6a3a2909
Kowalczyk, D <d.kowalczyk@tue.nl>
2020-07-17 21:28:02 +0200

    added new members zerohalf

--- include/ZeroHalfCuts.hpp
+++ include/ZeroHalfCuts.hpp
@@ -9,27 +9,33 @@
 #include "gurobi_c++.h"
 
 class ZeroHalfCuts {
-    ZeroHalfCuts(std::vector<BddCoeff>* _lp_sol, ReformulationModel* _rmp_model,
-                 NodeId _root, NodeTableEntity<>* _table);
-    ZeroHalfCuts(ZeroHalfCuts&&) = delete;
+   public:
+    ZeroHalfCuts(int _nb_jobs, int _nb_machines, ReformulationModel* _rmp_model,
+                 NodeId& _root, NodeTableEntity<>* _table);
+    ZeroHalfCuts(ZeroHalfCuts&&) = default;
     ZeroHalfCuts(const ZeroHalfCuts&) = delete;
-    ZeroHalfCuts& operator=(ZeroHalfCuts&&) = delete;
+    ZeroHalfCuts& operator=(ZeroHalfCuts&&) = default;
     ZeroHalfCuts& operator=(const ZeroHalfCuts&) = delete;
 
-    ~ZeroHalfCuts();
+    ~ZeroHalfCuts() = default;
     bool add_cuts();
+    void generate_cuts();
 
    private:
     std::unique_ptr<GRBEnv>   env;
     std::unique_ptr<GRBModel> model;
-    std::vector<BddCoeff>*    lp_sol;
+    int                       nb_jobs;
+    int                       nb_machines;
     ReformulationModel*       rmp_model;
-    NodeId                    root;
+    NodeId&                   root;
     NodeTableEntity<>*        table;
+    std::vector<GRBVar>       sigma{};
+    std::vector<NodeId>       node_ids{};
+    std::vector<GRBVar>       jobs_var;
+    int                       terminal_key;
 
     void generate_model();
-    void generate_cuts();
-    void init_dfs();
+    void dfs(const NodeId& v);
 };
 
 #endif  // __ZEROHALFCUTS_H__
