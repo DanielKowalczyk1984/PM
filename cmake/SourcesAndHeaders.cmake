e2108e4c71cde8c3fd3a0c29119f07e8f2e33fb2
Kowalczyk, D <d.kowalczyk@tue.nl>
2020-08-20 14:21:32 +0200

    deleted or_utils headers from SOurcesandHeaders

--- cmake/SourcesAndHeaders.cmake
+++ cmake/SourcesAndHeaders.cmake
@@ -1,17 +1,12 @@
 file(GLOB SOURCE_FILES
           "src/*.cc"
           "src/*.cpp"
-          src/alloc.c
-          src/binomial-heap.c
           src/branch_and_bound.c
           src/conflict_branching.c
-          src/copy.c src/graph.c
           src/greedy.c
-          src/heap.c
           src/interval.c
           src/io.c
           src/job.c
-          src/ksubset.c
           src/localsearch.c
           src/lowerbound.c
           src/lp.c
@@ -20,10 +15,6 @@ file(GLOB SOURCE_FILES
           src/preprocess.c
           src/scheduleset.c
           src/solution.c
-          src/sortus.c
-          src/time_bis.c
-          src/time.c
-          src/util.c
           src/wct.c
           src/wctparms.c
 )
@@ -34,7 +25,6 @@ set(sources
 
 set(exe_sources
 		src/mainwct.c
-		${sources}
 )
 
 set(headers
