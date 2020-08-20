59e05523862c4396a03bf1cf63126718eb236790
Kowalczyk, D <d.kowalczyk@tue.nl>
2020-08-20 16:25:50 +0200

    format simplification solver.h

--- include/solver.h
+++ include/solver.h
@@ -1,7 +1,8 @@
 #ifndef INCLUDE_SOLVER_H_
 #define INCLUDE_SOLVER_H_
 
-#include <scheduleset.h>
+// #include <scheduleset.h>
+#include <glib.h>
 #include <wctparms.h>
 
 #ifdef __cplusplus
@@ -9,20 +10,23 @@ extern "C" {
 #endif
 
 typedef struct PricerSolverBase PricerSolver;
-PricerSolver*                   newSolver(GPtrArray* jobs, int _num_machines,
-                                          GPtrArray* ordered_jobs, Parms* parms, int _Hmax,
-                                          int* _take_jobs);
-PricerSolver* newSolverDp(GPtrArray* _jobs, int _num_machines, int _Hmax,
-                          Parms* parms);
-PricerSolver* newSolverTIBdd(GPtrArray* _jobs, int _num_machines,
-                             GPtrArray* _ordered_jobs, int* _take_jobs,
-                             int _Hmax, Parms* parms);
-void          copy_solver(PricerSolver** dest, PricerSolver* src);
-void          freeSolver(PricerSolver* src);
-void          deletePricerSolver(PricerSolver* solver);
-int*          get_take(PricerSolver* solver);
 
-void iterate_dd(PricerSolver* solver);
+PricerSolver* newSolver(GPtrArray* jobs,
+                        int        _num_machines,
+                        GPtrArray* ordered_jobs,
+                        Parms*     parms,
+                        int        _Hmax,
+                        int*       _take_jobs);
+
+PricerSolver* newSolverDp(GPtrArray* _jobs,
+                          int        _num_machines,
+                          int        _Hmax,
+                          Parms*     parms);
+
+void freeSolver(PricerSolver* src);
+void deletePricerSolver(PricerSolver* solver);
+int* get_take(PricerSolver* solver);
+
 void iterate_zdd(PricerSolver* solver);
 
 int    get_num_layers(PricerSolver* solver);
@@ -32,8 +36,6 @@ size_t get_nb_edges(PricerSolver* solver);
 void print_number_paths(PricerSolver* solver);
 void print_dot_file(PricerSolver* solver, char* name);
 void print_number_nodes_edges(PricerSolver* solver);
-
-double get_edge_cost(PricerSolver* solver, int idx);
 
 #ifdef __cplusplus
 }
