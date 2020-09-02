9ba3a13189865da8b977153a4acf66688cd50b70
Kowalczyk, D <d.kowalczyk@tue.nl>
2020-09-02 10:04:47 +0200

    new constructor functions solver in wrapper C

--- src/pricersolverwrapper.cpp
+++ src/pricersolverwrapper.cpp
@@ -14,66 +14,70 @@ PricerSolverBase* newSolver(GPtrArray* jobs,
                             GPtrArray* ordered_jobs,
                             parms*     parms,
                             int        _Hmax,
-                            int*       _take_jobs) {
+                            int*       _take_jobs,
+                            double     _UB) {
     switch (parms->pricing_solver) {
         case bdd_solver_simple:
             return new PricerSolverBddSimple(jobs, _num_machines, ordered_jobs,
-                                             parms->pname, _Hmax, _take_jobs);
+                                             parms->pname, _Hmax, _take_jobs,
+                                             _UB);
             break;
         case bdd_solver_cycle:
             return new PricerSolverBddCycle(jobs, _num_machines, ordered_jobs,
-                                            parms->pname, _Hmax, _take_jobs);
+                                            parms->pname, _Hmax, _take_jobs,
+                                            _UB);
             break;
         case zdd_solver_cycle:
             return new PricerSolverZddCycle(jobs, _num_machines, ordered_jobs,
-                                            parms->pname);
+                                            parms->pname, _UB);
             break;
         case zdd_solver_simple:
             return new PricerSolverSimple(jobs, _num_machines, ordered_jobs,
-                                          parms->pname);
+                                          parms->pname, _UB);
             break;
         case bdd_solver_backward_simple:
             return new PricerSolverBddBackwardSimple(jobs, _num_machines,
                                                      ordered_jobs, parms->pname,
-                                                     _Hmax, _take_jobs);
+                                                     _Hmax, _take_jobs, _UB);
             break;
         case bdd_solver_backward_cycle:
             return new PricerSolverBddBackwardCycle(jobs, _num_machines,
                                                     ordered_jobs, parms->pname,
-                                                    _Hmax, _take_jobs);
+                                                    _Hmax, _take_jobs, _UB);
             break;
         case zdd_solver_backward_simple:
             return new PricerSolverZddBackwardSimple(
-                jobs, _num_machines, ordered_jobs, parms->pname);
+                jobs, _num_machines, ordered_jobs, parms->pname, _UB);
             break;
         case zdd_solver_backward_cycle:
-            return new PricerSolverZddBackwardCycle(jobs, _num_machines,
-                                                    ordered_jobs, parms->pname);
+            return new PricerSolverZddBackwardCycle(
+                jobs, _num_machines, ordered_jobs, parms->pname, _UB);
         default:
             return new PricerSolverBddBackwardCycle(jobs, _num_machines,
                                                     ordered_jobs, parms->pname,
-                                                    _Hmax, _take_jobs);
+                                                    _Hmax, _take_jobs, _UB);
     }
 }
 
 PricerSolverBase* newSolverDp(GPtrArray* _jobs,
                               int        _num_machines,
                               int        _Hmax,
-                              parms*     parms) {
+                              parms*     parms,
+                              double     _UB) {
     switch (parms->pricing_solver) {
         case dp_solver:
             return new PricerSolverSimpleDp(_jobs, _num_machines, _Hmax,
-                                            parms->pname);
+                                            parms->pname, _UB);
             break;
         case ati_solver:
             return new PricerSolverArcTimeDp(_jobs, _num_machines, _Hmax,
-                                             parms->pname);
+                                             parms->pname, _UB);
         case dp_bdd_solver:
             return new PricerSolverSimpleDp(_jobs, _num_machines, _Hmax,
-                                            parms->pname);
+                                            parms->pname, _UB);
         default:
             return new PricerSolverSimpleDp(_jobs, _num_machines, _Hmax,
-                                            parms->pname);
+                                            parms->pname, _UB);
             break;
     }
 }
@@ -132,7 +136,7 @@ int evaluate_nodes(NodeData* pd) {
 int reduce_cost_fixing(NodeData* pd) {
     int    val = 0;
     int    UB = pd->problem->opt_sol->tw;
-    double LB = pd->eta_in;
+    double LB = pd->LP_lower_bound_dual;
 
     pd->solver->reduce_cost_fixing(&g_array_index(pd->pi, double, 0), UB, LB);
     pd->problem->size_graph_after_reduced_cost_fixing =
@@ -154,6 +158,8 @@ int construct_lp_sol_from_rmp(NodeData* pd) {
 
     val = wctlp_get_nb_cols(pd->RMP, &nb_cols);
     CCcheck_val_2(val, "Failed to get nb cols");
+    assert(nb_cols - pd->id_pseudo_schedules == pd->localColPool->len);
+
     pd->lambda =
         CC_SAFE_REALLOC(pd->lambda, nb_cols - pd->id_pseudo_schedules, double);
     CCcheck_NULL_2(pd->lambda, "Failed to allocate memory to pd->x");
