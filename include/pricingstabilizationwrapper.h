d214f1ffa7be6cfbf8908d399a9c2eb0f0f53340
Kowalczyk, D <d.kowalczyk@tue.nl>
2020-09-10 09:34:54 +0200

    update header pricing

--- include/pricingstabilizationwrapper.h
+++ include/pricingstabilizationwrapper.h
@@ -14,12 +14,12 @@ typedef struct PricingStabilizationBase PricingStabilization;
 
 PricingStabilization* new_pricing_stabilization(PricerSolver* solver,
                                                 Parms*        parms);
-void   delete_pricing_stabilization(PricingStabilization* pricing_stab_solver);
+void   delete_pricing_stabilization(PricingStabilization* pricing_stabilization);
 double call_get_eta_in(PricingStabilization* pricing_stabilization);
-double call_get_reduced_cost(PricingStabilization* p);
-int    call_stopping_criteria(PricingStabilization* p);
-int    call_get_update_stab_center(PricingStabilization* p);
-void   call_update_duals(PricingStabilization* solver);
+double call_get_reduced_cost(PricingStabilization* pricing_stabilization);
+int    call_stopping_criteria(PricingStabilization* pricing_stabilization);
+int    call_get_update_stab_center(PricingStabilization* pricing_stabilization);
+void   call_update_duals(PricingStabilization* pricing_stabilization);
 #ifdef __cplusplus
 }
 #endif  // __cplusplus
