451768ef5fe45e3cbd40d8790dc58cd54407cc00
Kowalczyk, D <d.kowalczyk@tue.nl>
2021-03-06 00:04:33 +0100

    better Sources and Headers

--- cmake/SourcesAndHeaders.cmake
+++ cmake/SourcesAndHeaders.cmake
@@ -1,23 +1,40 @@
-file(
-  GLOB
-  SOURCE_FILES
-  "src/*.cpp"
-  # src/Statistics.c
-  # src/greedy.c
-  src/interval.c
-  # src/io.c
-  src/job.c
-  src/localsearch.c
-  # src/lowerbound.c
-  src/lp.c
-  src/model.c
-  src/partlist.c
-  # src/preprocess.c
-  src/scheduleset.c
-  src/solution.c
-  # src/wct.c
-  # src/wctparms.c
-  )
+set(SOURCE_FILES
+    src/BranchBoundTree.cpp
+    src/BranchNode.cpp
+    src/greedy.cpp
+    src/Instance.cpp
+    src/interval.cpp
+    src/Interval_new.cpp
+    src/io.cpp
+    src/job.c
+    src/localsearch.c
+    src/LocalSearch_new.cpp
+    src/lowerbound.cpp
+    src/model.cpp
+    src/ModelInterface.cpp
+    src/parms.cpp
+    src/partlist.c
+    src/preprocess.cpp
+    src/PricerSolverArcTimeDP.cpp
+    src/PricerSolverBase.cpp
+    src/PricerSolverBdd.cpp
+    src/PricerSolverBddBackward.cpp
+    src/PricerSolverBddForward.cpp
+    src/PricerSolverSimpleDP.cpp
+    src/PricerSolverWrappers.cpp
+    src/PricerSolverZdd.cpp
+    src/PricerSolverZddBackward.cpp
+    src/PricerSolverZddForward.cpp
+    src/PricingStabilization.cpp
+    src/scheduleset.cpp
+    src/SeperationSolver.cpp
+    src/solution.c
+    src/Solution_new.cpp
+    src/StabilizationWrappers.cpp
+    src/Statistics.cpp
+    src/wct.cpp
+    src/wctprivate.cpp
+    src/ZeroHalfCuts.cpp)
 
 set(sources ${SOURCE_FILES})
 
