c471c2060430ee164b11532ad6f6fca57a71f05d
Kowalczyk, D <d.kowalczyk@tue.nl>
2021-03-09 12:11:41 +0100

    problem constructor updated

--- src/wctprivate.cpp
+++ src/wctprivate.cpp
@@ -6,6 +6,7 @@
 #include <memory>
 #include <type_traits>
 #include <vector>
+#include "Instance.h"
 #include "LocalSearch_new.h"
 #include "PricerSolverArcTimeDP.hpp"
 #include "PricerSolverBddBackward.hpp"
@@ -18,28 +19,29 @@
 // #include "interval.h"
 
 Problem::~Problem() { /*free the parameters*/
-    g_ptr_array_free(g_job_array, TRUE);
-    g_ptr_array_free(intervals, TRUE);
-    solution_free(&(opt_sol));
+    // g_ptr_array_free(g_job_array, TRUE);
+    // g_ptr_array_free(intervals, TRUE);
+    // solution_free(&(opt_sol));
 }
 
 Problem::Problem(int argc, const char** argv)
-    : parms(),
+    : parms(argc, argv),
       stat(),
+      instance(&parms),
       tree(),
       root_pd(std::make_unique<NodeData>(this)),
-      g_job_array(g_ptr_array_new_with_free_func(g_job_free)),
+      //   g_job_array(g_ptr_array_new_with_free_func(g_job_free)),
       nb_jobs(),
       nb_machines(),
-      p_sum(),
-      pmax(),
-      pmin(std::numeric_limits<int>::max()),
-      dmax(std::numeric_limits<int>::min()),
-      dmin(std::numeric_limits<int>::max()),
-      H_min(),
-      H_max(std::numeric_limits<int>::max()),
-      off(),
-      intervals(g_ptr_array_new_with_free_func(g_interval_free)),
+      //   p_sum(),
+      //   pmax(),
+      //   pmin(std::numeric_limits<int>::max()),
+      //   dmax(std::numeric_limits<int>::min()),
+      //   dmin(std::numeric_limits<int>::max()),
+      //   H_min(),
+      //   H_max(std::numeric_limits<int>::max()),
+      //   off(),
+      //   intervals(g_ptr_array_new_with_free_func(g_interval_free)),
       /*B&B info*/
       global_upper_bound(std::numeric_limits<int>::max()),
       global_lower_bound(),
@@ -48,11 +50,8 @@ Problem::Problem(int argc, const char** argv)
       root_lower_bound(),
       root_rel_error(std::numeric_limits<double>::max()),
       status(no_sol),
-      opt_sol(nullptr) {
-    double start_time = 0.0;
-
+      opt_sol() {
     int val = program_header(argc, argv);
-    val = parms.parse_cmd(argc, argv);
 
     if (dbg_lvl() > 1) {
         fmt::print("Debugging turned on\n");
@@ -62,30 +61,22 @@ Problem::Problem(int argc, const char** argv)
      * @brief Reading and preprocessing the data
      *
      */
-    start_time = CCutil_zeit();
-    fs::path _path{parms.jobfile};
-    instance = Instance(_path, &parms);
-    problem_read();
-    preprocess_data();
-    fmt::print("Reading and preprocessing of the data took {} seconds\n",
-               CCutil_zeit() - start_time);
-    heuristic_new();
 
     /**
      *@brief Finding heuristic solutions to the problem or start without
      *feasible solutions
      */
     if (parms.use_heuristic) {
-        heuristic();
+        heuristic_new();
     } else {
-        opt_sol = solution_alloc(intervals->len, nb_machines, nb_jobs, off);
-        Solution* sol = opt_sol;
-        val = construct_edd(sol);
+        Sol best_sol(instance.nb_jobs, instance.nb_machines, instance.off);
+        best_sol.construct_edd(instance.jobs);
         fmt::print("Solution Constructed with EDD heuristic:\n");
-        solution_print(sol);
-        solution_canonical_order(sol, intervals);
+        best_sol.print_solution();
+        best_sol.canonical_order(instance.intervals);
         fmt::print("Solution in canonical order: \n");
-        solution_print(sol);
+        best_sol.print_solution();
+        opt_sol = best_sol;
     }
     /**
      * @brief Build DD at the root node
@@ -94,64 +85,45 @@ Problem::Problem(int argc, const char** argv)
     CCutil_start_timer(&(stat.tot_build_dd));
     switch (parms.pricing_solver) {
         case bdd_solver_simple:
-            root_pd->solver = std::make_unique<PricerSolverBddSimple>(
-                g_job_array, nb_machines, root_pd->ordered_jobs,
-                parms.pname.c_str(), H_max, nullptr, opt_sol->tw);
+            root_pd->solver = std::make_unique<PricerSolverBddSimple>(instance);
             break;
         case bdd_solver_cycle:
-            root_pd->solver = std::make_unique<PricerSolverBddCycle>(
-                g_job_array, nb_machines, root_pd->ordered_jobs,
-                parms.pname.c_str(), H_max, nullptr, opt_sol->tw);
+            root_pd->solver = std::make_unique<PricerSolverBddCycle>(instance);
             break;
         case zdd_solver_cycle:
-            root_pd->solver = std::make_unique<PricerSolverZddCycle>(
-                g_job_array, nb_machines, root_pd->ordered_jobs,
-                parms.pname.c_str(), global_upper_bound);
+            root_pd->solver = std::make_unique<PricerSolverZddCycle>(instance);
             break;
         case zdd_solver_simple:
-            root_pd->solver = std::make_unique<PricerSolverSimple>(
-                g_job_array, nb_machines, root_pd->ordered_jobs,
-                parms.pname.c_str(), opt_sol->tw);
+            root_pd->solver = std::make_unique<PricerSolverSimple>(instance);
             break;
         case bdd_solver_backward_simple:
-            root_pd->solver = std::make_unique<PricerSolverBddBackwardSimple>(
-                g_job_array, nb_machines, root_pd->ordered_jobs,
-                parms.pname.c_str(), H_max, nullptr, global_upper_bound);
+            root_pd->solver =
+                std::make_unique<PricerSolverBddBackwardSimple>(instance);
             break;
         case bdd_solver_backward_cycle:
-            root_pd->solver = std::make_unique<PricerSolverBddBackwardCycle>(
-                g_job_array, nb_machines, root_pd->ordered_jobs,
-                parms.pname.c_str(), H_max, nullptr, global_upper_bound);
+            root_pd->solver =
+                std::make_unique<PricerSolverBddBackwardCycle>(instance);
             ;
             break;
         case zdd_solver_backward_simple:
-            root_pd->solver = std::make_unique<PricerSolverZddBackwardSimple>(
-                g_job_array, nb_machines, root_pd->ordered_jobs,
-                parms.pname.c_str(), opt_sol->tw);
+            root_pd->solver =
+                std::make_unique<PricerSolverZddBackwardSimple>(instance);
             break;
         case zdd_solver_backward_cycle:
-            root_pd->solver = std::make_unique<PricerSolverZddBackwardCycle>(
-                g_job_array, nb_machines, root_pd->ordered_jobs,
-                parms.pname.c_str(), opt_sol->tw);
+            root_pd->solver =
+                std::make_unique<PricerSolverZddBackwardCycle>(instance);
         case dp_solver:
-            root_pd->solver = std::make_unique<PricerSolverSimpleDp>(
-                g_job_array, nb_machines, H_max, parms.pname.c_str(),
-                opt_sol->tw);
+            root_pd->solver = std::make_unique<PricerSolverSimpleDp>(instance);
             break;
         case ati_solver:
-            root_pd->solver = std::make_unique<PricerSolverArcTimeDp>(
-                g_job_array, nb_machines, H_max, parms.pname.c_str(),
-                opt_sol->tw);
+            root_pd->solver = std::make_unique<PricerSolverArcTimeDp>(instance);
             break;
         case dp_bdd_solver:
-            root_pd->solver = std::make_unique<PricerSolverSimpleDp>(
-                g_job_array, nb_machines, H_max, parms.pname.c_str(),
-                opt_sol->tw);
+            root_pd->solver = std::make_unique<PricerSolverSimpleDp>(instance);
             break;
         default:
-            root_pd->solver = std::make_unique<PricerSolverBddBackwardCycle>(
-                g_job_array, nb_machines, root_pd->ordered_jobs,
-                parms.pname.c_str(), H_max, nullptr, global_upper_bound);
+            root_pd->solver =
+                std::make_unique<PricerSolverBddBackwardCycle>(instance);
             break;
     }
     CCutil_stop_timer(&(stat.tot_build_dd), 0);
@@ -185,7 +157,7 @@ Problem::Problem(int argc, const char** argv)
             break;
     }
     root_pd->stat = &(stat);
-    root_pd->opt_sol = opt_sol;
+    root_pd->opt_sol = &opt_sol;
 
     /**
      * @brief Initialization of the B&B tree
@@ -195,44 +167,46 @@ Problem::Problem(int argc, const char** argv)
     // tree->explore();
 }
 
-void Problem::calculate_Hmax() {
-    int       temp = 0;
-    double    temp_dbl = 0.0;
-    NodeData* pd = root_pd.get();
+// void Problem::calculate_Hmax() {
+//     int       temp = 0;
+//     double    temp_dbl = 0.0;
+//     NodeData* pd = root_pd.get();
 
-    temp = p_sum - pmax;
-    temp_dbl = static_cast<double>(temp);
-    temp_dbl = floor(temp_dbl / nb_machines);
-    H_max = pd->H_max = static_cast<int>(temp_dbl) + pmax;
-    H_min = pd->H_min = static_cast<int>(ceil(temp_dbl / nb_machines)) - pmax;
+//     temp = p_sum - pmax;
+//     temp_dbl = static_cast<double>(temp);
+//     temp_dbl = floor(temp_dbl / nb_machines);
+//     H_max = pd->H_max = static_cast<int>(temp_dbl) + pmax;
+//     H_min = pd->H_min = static_cast<int>(ceil(temp_dbl / nb_machines)) -
+//     pmax;
 
-    GPtrArray* duration = g_ptr_array_copy(g_job_array, NULL, NULL);
-    g_ptr_array_set_free_func(duration, NULL);
-    g_ptr_array_sort(duration, g_compare_duration);
+//     GPtrArray* duration = g_ptr_array_copy(g_job_array, NULL, NULL);
+//     g_ptr_array_set_free_func(duration, NULL);
+//     g_ptr_array_sort(duration, g_compare_duration);
 
-    int    m = 0;
-    int    i = nb_jobs - 1;
-    double tmp = p_sum;
-    H_min = p_sum;
-    do {
-        Job* job = static_cast<Job*>(g_ptr_array_index(duration, i));
-        tmp -= job->processing_time;
-        m++;
-        i--;
+//     int    m = 0;
+//     int    i = nb_jobs - 1;
+//     double tmp = p_sum;
+//     H_min = p_sum;
+//     do {
+//         Job* job = static_cast<Job*>(g_ptr_array_index(duration, i));
+//         tmp -= job->processing_time;
+//         m++;
+//         i--;
 
-    } while (m < nb_machines - 1);
+//     } while (m < nb_machines - 1);
 
-    H_min = pd->H_min = static_cast<int>(ceil(tmp / nb_machines));
-    g_ptr_array_free(duration, TRUE);
-    fmt::print(
-        R"(H_max = {}, H_min = {},  pmax = {}, pmin = {}, p_sum = {}, off = {}
-)",
-        H_max, H_min, pmax, pmin, p_sum, off);
-}
+//     H_min = pd->H_min = static_cast<int>(ceil(tmp / nb_machines));
+//     g_ptr_array_free(duration, TRUE);
+//     fmt::print(
+//         R"(H_max = {}, H_min = {},  pmax = {}, pmin = {}, p_sum = {}, off =
+//         {}
+// )",
+//         H_max, H_min, pmax, pmin, p_sum, off);
+// }
 
 NodeData::~NodeData() {
     temporary_data_free();
-    g_ptr_array_free(ordered_jobs, TRUE);
+    // g_ptr_array_free(ordered_jobs, TRUE);
     // g_ptr_array_free(best_schedule, TRUE);
 }
 
