bd40d6c01c689e4423b8385b48d8db7ce9b21428
Kowalczyk, D <d.kowalczyk@tue.nl>
2021-03-11 09:52:25 +0100

    cleanup Job

--- include/Job.h
+++ include/Job.h
@@ -1,13 +1,8 @@
 #ifndef JOB_H
 #define JOB_H
 
-// #ifdef __cplusplus
-// extern "C" {
-// #endif
-// #include <glib.h>
-// #include <util.h>
-
 #include <algorithm>
+#include <cstddef>
 struct Job {
     int job{};
     int weight{};
@@ -23,7 +18,6 @@ struct Job {
     Job& operator=(const Job&) = default;
     Job& operator=(Job&&) = default;
     ~Job() = default;
-    // int* pos_interval;
 };
 
 inline int value_Fj(int C, Job* j) {
@@ -32,18 +26,5 @@ inline int value_Fj(int C, Job* j) {
 int value_diff_Fij(int C, Job* i, Job* j);
 int bool_diff_Fij(int, Job*, Job*);
 int arctime_diff_Fij(int weight, Job* i, Job* j);
-
-// Job* job_alloc(int* p, int* w, int* d);
-// void g_job_free(void* set);
-// void job_init(Job* job, int p, int d, int w);
-
-// void g_set_jobarray_job(gpointer data, gpointer user_data);
-// void g_print_jobarray(gpointer data, gpointer user_data);
-// void g_print_machine(gpointer data, gpointer user_data);
-// void g_print_job(gpointer data, gpointer user_data);
-
-// #ifdef __cplusplus
-// }
-// #endif
 
 #endif  // JOB_H
