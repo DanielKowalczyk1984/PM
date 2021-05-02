867a2f0c507128688589e9bd5b043f5e364d3a96
Kowalczyk, D <d.kowalczyk@tue.nl>
2021-05-02 12:12:17 +0200

    modelinterface size_t job

--- src/ModelInterface.cpp
+++ src/ModelInterface.cpp
@@ -4,12 +4,15 @@
 #include <range/v3/view/iota.hpp>
 #include <range/v3/view/transform.hpp>
 
-ReformulationModel::ReformulationModel(int nb_assignments, int nb_machines)
+ReformulationModel::ReformulationModel(size_t nb_assignments,
+                                       size_t nb_machines)
     : std::vector<std::shared_ptr<ConstraintBase>>(
-          ranges::views::iota(0, nb_assignments) |
-          ranges::views::transform(
-              [](int i) { return std::make_shared<ConstraintAssignment>(i); }) |
+          ranges::views::iota(0UL, nb_assignments) |
+          ranges::views::transform([](auto i) {
+              return std::make_shared<ConstraintAssignment>(i);
+          }) |
           ranges::to<std::vector<std::shared_ptr<ConstraintBase>>>()) {
+    auto m = static_cast<double>(nb_machines);
     (*this).push_back(
-        std::make_shared<ConstraintConvex>(static_cast<double>(-nb_machines)));
+        std::make_shared<ConstraintConvex>(static_cast<double>(-m)));
 }
