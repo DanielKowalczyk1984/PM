fe0215ac985f7c5e4615dbef92212a7ebbd428d5
Kowalczyk, D <d.kowalczyk@tue.nl>
2021-06-23 08:35:37 +0200

    removed branch history

--- include/NodeBdd.hpp
+++ include/NodeBdd.hpp
@@ -6,7 +6,6 @@
 #include <boost/dynamic_bitset.hpp>
 #include <cstddef>
 #include <memory>
-#include "BranchHistory.hpp"
 #include "Label.hpp"
 #include "ModelInterface.hpp"
 #include "NodeBase.hpp"
@@ -14,9 +13,8 @@
 template <typename T = double>
 class NodeBdd : public NodeBase {
    private:
-    Job*                           job{nullptr};
-    int                            weight{};
-    std::shared_ptr<BranchHistory> br_history{};
+    Job* job{nullptr};
+    int  weight{};
 
    public:
     std::array<Label<NodeBdd<T>, T>, 2>                 forward_label{};
