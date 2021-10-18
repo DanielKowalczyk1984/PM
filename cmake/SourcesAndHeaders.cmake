57f174f3aafe6f73c6e637838d5181eb38a23596
Kowalczyk, D <d.kowalczyk@tue.nl>
2021-10-19 00:10:46 +0200

    update cmake pm

--- cmake/SourcesAndHeaders.cmake
+++ cmake/SourcesAndHeaders.cmake
@@ -35,6 +35,6 @@ set(sources
         "src/ZeroHalfSystem.cpp"
         )
 
-set(exe_sources "src/main.cpp")
+set(exe_sources "src/main.cpp" ${sources})
 
 set(headers ${PROJECT_SOURCE_DIR}/include)
