8cc9dc7bf1f65d9d5edc4a02bad74972b3efc60c
Kowalczyk, D <d.kowalczyk@tue.nl>
2021-11-11 13:17:41 +0100

    test new before script

--- before_script.sh
+++ before_script.sh
@@ -1,26 +1,41 @@
 #!/bin/bash
 
-if [ ! -d "./ThirdParty/Cgl" ]; then
-	mkdir ThirdParty
-	wget -O ThirdParty/coinbrew https://raw.githubusercontent.com/coin-or/coinbrew/master/coinbrew
-	chmod u+x ThirdParty/coinbrew
-	cd ThirdParty
+export CMAKE_VERSION="3.21.4"
+
+mkdir -p ThirdParty
+cd ThirdParty
+
+if [ ! -d "./cmake"]; then
+	wget -O cmake-${VERSION_CMAKE}.tar.gz https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}.tar.gz
+	tar -zxf cmake-${CMAKE_VERSION}.tar.gz
+	rm cmake-${CMAKE_VERSION}.tar.gz
+	mv cmake-${CMAKE_VERSION} cmake
+	cd cmake
+	./bootstrap
+	make
+else
+	cd make 	
+fi
+
+make install && cd ..
+
+if [ ! -d "./Cgl" ]; then
+	wget -O coinbrew https://raw.githubusercontent.com/coin-or/coinbrew/master/coinbrew
+	chmod u+x coinbrew
 	export CC=gcc-11
 	export CXX=g++-11
 	./coinbrew fetch Cgl@master
 	if [ ! -d "./coin-or-x64-linux-release" ]; then
-		./coinbrew build Cgl --no-prompt --prefix="coin-or-x64-linux-release" --with-gurobi-lflags="-L$GUROBI_HOME/lib -lgurobi91 -lpthread -lm" --with-gurobi-cflags="-I$GUROBI_HOME/include" --tests none	
+		./coinbrew build Cgl --no-prompt --prefix="coin-or-x64-linux-release" --with-gurobi-lflags="-L$GUROBI_HOME/lib -lgurobi91 -lpthread -lm" --with-gurobi-cflags="-I$GUROBI_HOME/include" --tests none
 	fi
 
-	if [ ! -d "./coin-or-x64-linux-debug" ]; then 
+	if [ ! -d "./coin-or-x64-linux-debug" ]; then
 		./coinbrew build Cgl --no-prompt --prefix="coin-or-x64-linux-debug" --reconfigure --enable-debug --with-gurobi-lflags="-L$GUROBI_HOME/lib -lgurobi91 -lpthread -lm" --with-gurobi-cflags="-I$GUROBI_HOME/include" --tests none
 	fi
-	cd ..
-
 fi
 
-if [ ! -d "./ThirdParty/vcpkg" ]; then
-	git clone https://github.com/Microsoft/vcpkg.git ThirdParty/vcpkg
-	./ThirdParty/vcpkg/bootstrap-vcpkg.sh --useSystemBinaries
-	./ThirdParty/vcpkg/vcpkg install range-v3 fmt boost-chrono boost-timer boost-atomic boost-json boost-random boost-regex boost-serialization boost-multiprecision boost-graph docopt nlohmann-json
+if [ ! -d "./vcpkg" ]; then
+	git clone https://github.com/Microsoft/vcpkg.git vcpkg
+	./vcpkg/bootstrap-vcpkg.sh --useSystemBinaries
+	./vcpkg/vcpkg install range-v3 fmt boost-chrono boost-timer boost-atomic boost-json boost-random boost-regex boost-serialization boost-multiprecision boost-graph docopt nlohmann-json
 fi
