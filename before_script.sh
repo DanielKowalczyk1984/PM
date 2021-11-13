c7c8096238da8ecee0ec86c38bb9e2f75b4ec3c6
Kowalczyk, D <d.kowalczyk@tue.nl>
2021-11-13 21:38:21 +0100

    added cmake

--- before_script.sh
+++ before_script.sh
@@ -3,24 +3,25 @@
 export CMAKE_VERSION="3.21.4"
 export GRB_VERSION="9.1.2_linux64"
 export GRB_SHORT_VERSION="9.1"
+export GUROBI_HOME="/opt/gurobi/linux64"
 
 mkdir -p ThirdParty
 cd ThirdParty
 
-#if [ ! -d "./cmake" ]; then
-#	wget https://github.com/Kitware/CMake/releases/download/v$CMAKE_VERSION/cmake-$CMAKE_VERSION.tar.gz
-#	tar -zxf cmake-$CMAKE_VERSION.tar.gz
-#	rm cmake-$CMAKE_VERSION.tar.gz
-#	mv cmake-$CMAKE_VERSION cmake
-#	cd cmake
-#	./bootstrap
-#	make
-#else
-#	cd cmake
-#fi
+if [ ! -d "./cmake" ]; then
+	wget https://github.com/Kitware/CMake/releases/download/v$CMAKE_VERSION/cmake-$CMAKE_VERSION.tar.gz
+	tar -zxf cmake-$CMAKE_VERSION.tar.gz
+	rm cmake-$CMAKE_VERSION.tar.gz
+	mv cmake-$CMAKE_VERSION cmake
+	cd cmake
+	./bootstrap
+	make
+else
+	cd cmake
+fi
 
-# make install
-# cd ..
+make install
+cd ..
 
 if [ ! -d "./Cgl" ]; then
 	wget -O coinbrew https://raw.githubusercontent.com/coin-or/coinbrew/master/coinbrew
