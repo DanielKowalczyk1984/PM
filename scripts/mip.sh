5afd7ff1ad1818a62732cbbdf90b304ad4d62c6c
Kowalczyk, D <daniel.kowalczyk1984@gmail.com>
2021-12-14 16:16:02 +0100

    update mip.sh json

--- scripts/mip.sh
+++ scripts/mip.sh
@@ -7,10 +7,10 @@ SRC_DIR=$(dirname "$BASH_SOURCE")/..
 # Invoke MIP on the rdlp instance files in paralel. All output is collected in a single output file
 # Only output lines marked with a 'tag' are preserved in the final output. This is convenient to remove debug output.
 function runBenchmark() {
-    instances=($(fd --regex "wt(040|050).*[16]\.dat" .))
+    instances=($(fd --regex "wt(040|050|100).*[16]\.dat" .))
     machines=(4 2)
     solvers=4
-    printf "%s\n" "${machines[@]}" | xargs -I{} printf "%s {}\n" "${instances[@]}" | parallel --no-notice -P 3 --eta --colsep ' ' "build/bin/PM -s 3 -f 15000 -S 1 --branching_point 0.5 {}"
+    printf "%s\n" "${machines[@]}" | xargs -I{} printf "%s {}\n" "${instances[@]}" | parallel --no-notice -P 25 --eta --colsep ' ' "build/PM --json settings/default_settings.json {}"
 }
 
 #switch to the root directory. This allows us to invoke this script from any directory. Then run the benchmark.
