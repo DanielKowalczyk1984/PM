9f009bf5a38cc2f5f34e6e611deb6f65f761ac62
Daniel Kowalczyk <daniel.kowalczyk1984@gmail.com>
2021-12-16 21:24:03 +0100

    default iterate

--- scripts/mip.sh
+++ scripts/mip.sh
@@ -9,8 +9,8 @@ SRC_DIR=$(dirname "$BASH_SOURCE")/..
 function runBenchmark() {
     instances=($(fd --regex "wt(040|050|100).*[16]\.dat" .))
     machines=(4 2)
-    solvers=4
-    printf "%s\n" "${machines[@]}" | xargs -I{} printf "%s {}\n" "${instances[@]}" | parallel --no-notice -P 25 --eta --colsep ' ' "build/PM --json settings/default_settings.json {}"
+    settings=("settings/default_settings_8.json" "settings/default_settings_10.json" "settings/default_settings_12.json")
+    printf "%s\n" "${machines[@]}" | xargs -I{} printf "%s {}\n" "${instances[@]}"| xargs -I{} printf "--json %s {}\n" "${settings[@]}" | parallel --no-notice -P 35 --eta --colsep ' ' "build/PM {}"
 }
 
 #switch to the root directory. This allows us to invoke this script from any directory. Then run the benchmark.
