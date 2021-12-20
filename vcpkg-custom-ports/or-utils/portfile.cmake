f8c2f6fece5fb55774a44151e4d48415b8948358
Kowalczyk, D <daniel.kowalczyk1984@gmail.com>
2021-12-20 13:11:22 +0100

    update portfile or-utils

--- vcpkg-custom-ports/or-utils/portfile.cmake
+++ vcpkg-custom-ports/or-utils/portfile.cmake
@@ -2,19 +2,23 @@
 vcpkg_from_gitlab(
     GITLAB_URL https://gitlab.kuleuven.be
     OUT_SOURCE_PATH SOURCE_PATH
-    REF 5e8d36decd0b374aa8c51231292711ddd85de0fb
-    SHA512 6b5cabc98eb111e48eb00a168af077f6f0d60cebaf14bda4e4bc66f4e1c6d33ea34c9e078d0020cd3db607cab1a1b0058740ef1457247b4241969598983e2ed7
+    REF 22b44eb1155428973f57937a989692ad55ba7ef5
+    SHA512 310ce569ff47b270bede1181ed5d4f445cff1f47b3121b1e6c4a97c8684c358dafae10d1d21e3611041adc35e1123b59f71e21cc2b3e74168e36d7fed82c8a99
     REPO u0056096/or-utils
     HEAD_REF develop
 )
 
 set(ENV{GUROBI_HOME} "C:/gurobi912/win64")
-
-vcpkg_configure_cmake(
+# if (VCPKG_LVIBRARY_LINKAGE STREQUAL dynamic)
+#     message(STATUS "Warning: Dynamic building not supported yet. Building static.")
+#     set(VCPKG_LIBRARY_LINKAGE static)
+# endif()
+vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
 )
 
-vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets()
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+vcpkg_copy_pdbs()
