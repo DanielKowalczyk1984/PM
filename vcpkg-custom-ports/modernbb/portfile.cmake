0abfe52e51b0060370ac0a359a7bd4d2a9c7464c
Kowalczyk, D <daniel.kowalczyk1984@gmail.com>
2021-12-23 15:30:41 +0100

    new version bb

--- vcpkg-custom-ports/modernbb/portfile.cmake
+++ vcpkg-custom-ports/modernbb/portfile.cmake
@@ -2,8 +2,8 @@
 vcpkg_from_gitlab(
     GITLAB_URL https://gitlab.kuleuven.be
     OUT_SOURCE_PATH SOURCE_PATH
-    REF v0.1.1
-    SHA512 11b7b6ec26045ffbe0c5c84e79ddee3baed8183829a3cc452fe590f7270afeef703c319ca8d7528f96ec3f8e6436586bf82c25abf70c3018ef959d1cccaf9a41
+    REF v0.1.2
+    SHA512 9cc87c073ad3e3b337a14327aae1a2aeffda1e40c3102c27e91d291f1e1124aa4fa71f428d5cf4f48ceb7b668a224891de2b8571bf05a04b6a905fdd450fa4f3
     REPO u0056096/branch-and-bound
 )
 
