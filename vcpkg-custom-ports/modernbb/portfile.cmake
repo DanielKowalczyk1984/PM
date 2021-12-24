5d640348a0185833018dff2c4fc6076b9e03a037
Kowalczyk, D <daniel.kowalczyk1984@gmail.com>
2021-12-24 15:22:37 +0100

    once again

--- vcpkg-custom-ports/modernbb/portfile.cmake
+++ vcpkg-custom-ports/modernbb/portfile.cmake
@@ -12,7 +12,7 @@ vcpkg_cmake_configure(
     PREFER_NINJA
 )
 
-vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(PACKAGE_NAME "branch-and-bound")
+vcpkg_cmake_install()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 vcpkg_copy_pdbs()
