5eaf6aed55b2dbd7200224f5af1e81e87a5321b0
Kowalczyk, D <daniel.kowalczyk1984@gmail.com>
2021-12-24 15:35:42 +0100

    portfiles

--- vcpkg-custom-ports/moderndd/portfile.cmake
+++ vcpkg-custom-ports/moderndd/portfile.cmake
@@ -12,7 +12,7 @@ vcpkg_cmake_configure(
     PREFER_NINJA
 )
 
-vcpkg_cmake_config_fixup(PACKAGE_NAME "ModernDD")
 vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(PACKAGE_NAME "ModernDD")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 vcpkg_copy_pdbs()
